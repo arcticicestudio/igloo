ebc157462608f3cbbb64341ba43977ec028cda4c
Arctic Ice Studio <development@arcticicestudio.com>
2020-10-08 22:09:11 +0200

    Preserve executability with `cpr` alias
    To preserve the executability when using the `cpr` alias the `--executability` will be added.
    
    GH-288

--- snowblocks/zsh/pkgs/rsync/aliases.zsh
+++ snowblocks/zsh/pkgs/rsync/aliases.zsh
@@ -6,5 +6,5 @@
 # License:    MIT
 
 # rsync based file system operations with detailed process and status information.
-alias cpr='rsync --archive -hh --partial --info=name2 --info=progress2 --info=stats1 --modify-window=1'
+alias cpr='rsync --archive --executability -hh --partial --info=name2 --info=progress2 --info=stats1 --modify-window=1'
 alias mvr='cpr --remove-source-files'
