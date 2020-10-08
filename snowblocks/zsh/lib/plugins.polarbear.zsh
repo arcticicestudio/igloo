6fe404ca0530aa22d8c0de696e0abb2c62236924
Arctic Ice Studio <development@arcticicestudio.com>
2020-10-08 22:19:27 +0200

    Fix missing root slashes in `fzf` absolute path of plugin loader script
    The plugin loader script of the `polarbear` host was missing leading `/`
    [2] in front of the two absolute fzf [2] paths that caused a plugin
    loading error.
    
    [1]: https://github.com/arcticicestudio/igloo/blob/18b73500/snowblocks/zsh/lib/plugins.polarbear.zsh#L13-L15
    [2]: https://github.com/junegunn/fzf
    
    GH-290

--- snowblocks/zsh/lib/plugins.polarbear.zsh
+++ snowblocks/zsh/lib/plugins.polarbear.zsh
@@ -10,9 +10,9 @@
 #   1. https://github.com/zplug/zplug
 [[ -f $ZDOTDIR/lib/plugins-base.zsh ]] && source $ZDOTDIR/lib/plugins-base.zsh
 
-zplug "usr/share/fzf", \
+zplug "/usr/share/fzf", \
   from:local, \
-  if:"type fzf > /dev/null && [[ -d usr/share/fzf ]]", \
+  if:"type fzf > /dev/null && [[ -d /usr/share/fzf ]]", \
   use:"{completion,key-bindings}.zsh"
 
 # Load all plugins and add commands to the executable search path.
